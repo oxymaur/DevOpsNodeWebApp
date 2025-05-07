fcdb6e5c2d0ed3e982fd0a5b329306325fc4de70
oxymaur <saezmauricio@hotmail.com>
2025-05-07 23:50:17 +0200

    deployed automatically to render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Oxymaur was having fun here on a Wednesday night');
+  res.send('Oxymaur was having fun here on a Wednesday night, and are you automatically deployed on RENDER NOW????');
 });
 
 app.listen(PORT, HOST);
